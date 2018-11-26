6fd5a430ee5f83a15223346512986b7e4ca58c96
Hussein <tejanahmedhu@gmail.com>
2018-11-26 00:59:27 +0000

    typescript version of test.js ES6 code

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-class Person{
-    sayHiTo(name){
-        console.log("Hi ", name);
+var Person = /** @class */ (function () {
+    function Person() {
     }
-}
-
+    Person.prototype.sayHiTo = function (name) {
+        console.log("Hi ", name);
+    };
+    return Person;
+}());
 var receptionist = new Person();
 receptionist.sayHiTo("Johnny Bravo");
---
+++ test.ts
@@ -0,0 +1,8 @@
+class Person{
+    sayHiTo(name){
+        console.log("Hi ", name);
+    }
+}
+
+var receptionist = new Person();
+receptionist.sayHiTo("Johnny Bravo");
